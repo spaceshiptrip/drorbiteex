7c7871be6ef31980fa2f6bc707992dbcff08a113
spaceshiptrip <4379326+spaceshiptrip@users.noreply.github.com>
2025-08-08 00:58:33 -0700

    add station name to polar plot

--- src/main/java/eu/dariolucia/drorbiteex/fxml/PolarPlot.java
+++ src/main/java/eu/dariolucia/drorbiteex/fxml/PolarPlot.java
@@ -305,6 +305,8 @@ public class PolarPlot implements Initializable {
 
     public void selectGroundStation(GroundStationGraphics b) {
         this.currentMask = b.getGroundStation().getMask() != null ? b.getGroundStation().getMask().getAzElMap() : null;
+        setText(PlotPosition.BOTTOM_LEFT, b.getGroundStation().getName() + " (" + b.getGroundStation().getCode() + ")", Color.WHITE);
+
         refresh();
     }
 
